954376877804c3cd6af9142fbd3e0c8d5d91c8f8
Fabijan Bajo <bajo09@gmail.com>
2017-05-19 13:20:31 +0200

    Add MVVM Base

---
+++ Architectures/MVVM/TabBarViewController.swift
@@ -0,0 +1,34 @@
+//
+//  TabBarViewController.swift
+//  Architectures
+//
+//  Created by Fabijan Bajo on 19/05/2017.
+//
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    // MARK: - Methods
+    
+    
+    
+    // MARK: - Actions
+    
+    
+    
+    // MARK: - Navigation
+}
+
+}
