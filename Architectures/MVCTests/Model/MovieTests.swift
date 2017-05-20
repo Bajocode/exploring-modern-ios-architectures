8970eefcb44de0af56a0bcab37b93fe3af27609b
Fabijan Bajo <bajo09@gmail.com>
2017-05-20 17:58:48 +0200

    Add Movie object initialization tests

--- Architectures/MVCTests/Model/MovieTests.swift
+++ Architectures/MVCTests/Model/MovieTests.swift
@@ -28,7 +28,25 @@ class MovieTests: XCTestCase {
     // MARK: - Initialization
     
     func test_Init_WhenGivenTitle_SetsTitle() {
-        
+        let movie = Movie(title: "Foo", posterPath: "", movieID: 0, releaseDate: Date(), averageRating: 0.0)
+        XCTAssertEqual(movie.title, "Foo")
+    }
+    func test_Init_WhenGivenPosterPath_SetsPosterPath() {
+        let movie = Movie(title: "", posterPath: "Foo", movieID: 0, releaseDate: Date(), averageRating: 0.0)
+        XCTAssertEqual(movie.posterPath, "Foo")
+    }
+    func test_Init_WhenGivenID_SetsID() {
+        let movie = Movie(title: "", posterPath: "", movieID: 1, releaseDate: Date(), averageRating: 0.0)
+        XCTAssertEqual(movie.movieID, 1)
+    }
+    func test_Init_WhenGivenReleaseDate_SetsReleaseDate() {
+        let dateNow = Date()
+        let movie = Movie(title: "", posterPath: "", movieID: 0, releaseDate: dateNow, averageRating: 0.0)
+        XCTAssertEqual(movie.releaseDate, dateNow)
+    }
+    func test_Init_WhenGivenRating_SetsRating() {
+        let movie = Movie(title: "", posterPath: "", movieID: 0, releaseDate: Date(), averageRating: 1.0)
+        XCTAssertEqual(movie.averageRating, 1.0)
     }
     
 }
