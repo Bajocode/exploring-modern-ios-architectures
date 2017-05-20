b4333162332494657638f6201ee67a6d8734cb3b
Fabijan Bajo <bajo09@gmail.com>
2017-05-20 17:42:39 +0200

    Add unit testing framework to MVC Model

---
+++ Architectures/MVCTests/Model/MovieTests.swift
@@ -0,0 +1,34 @@
+//
+//  MovieTests.swift
+//  Architectures
+//
+//  Created by Fabijan Bajo on 20/05/2017.
+//
+//
+
+import XCTest
+@testable import MVC
+
+class MovieTests: XCTestCase {
+    
+    
+    // MARK: - Related to every test
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    // MARK: - Initialization
+    
+    func test_Init_WhenGivenTitle_SetsTitle() {
+        
+    }
+    
+}
