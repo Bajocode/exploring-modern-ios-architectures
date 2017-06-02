e83d110bb898e328787cf04dc608f4ff85809efe
Fabijan Bajo <bajo09@gmail.com>
2017-06-02 15:34:32 +0200

    MVVM + MVP: Add model tests

--- Architectures/MVCTests/Model/MovieTests.swift
+++ Architectures/MVCTests/Model/MovieTests.swift
@@ -12,15 +12,13 @@ import XCTest
 class MovieTests: XCTestCase {
     
     
-    // MARK: - Related to every test
+    // MARK: - Configuration
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
---
+++ Architectures/MVPTests/Model/MovieTests.swift
@@ -0,0 +1,52 @@
+//
+//  MovieTests.swift
+//  Architectures
+//
+//  Created by Fabijan Bajo on 02/06/2017.
+//
+//
+
+import XCTest
+@testable import MVP
+
+class MovieTests: XCTestCase {
+    
+    
+    // MARK: - Configuration
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    
+    // MARK: - Initializers
+    
+    func test_Init_WhenGivenValues_SetsValues() {
+        let movie = Movie(title: "Foo", posterPath: "Bar", movieID: 0, releaseDate: "Baz", averageRating: 1.0)
+        
+        XCTAssertEqual(movie.title, "Foo")
+        XCTAssertEqual(movie.posterPath, "Bar")
+        XCTAssertEqual(movie.movieID, 0)
+        XCTAssertEqual(movie.releaseDate, "Baz")
+        XCTAssertEqual(movie.averageRating, 1.0)
+    }
+    
+    
+    // MARK: - Equatable
+    
+    func test_EqualMovies_AreEqual_WithSameIDs() {
+        let first = Movie(title: "", posterPath: "", movieID: 0, releaseDate: "", averageRating: 0.0)
+        let second = Movie(title: "", posterPath: "", movieID: 0, releaseDate: "", averageRating: 0.0)
+        XCTAssertEqual(first, second)
+    }
+    func test_EqualMovies_AreUnEqual_WithDIfferentIDs() {
+        let first = Movie(title: "", posterPath: "", movieID: 0, releaseDate: "", averageRating: 0.0)
+        let second = Movie(title: "", posterPath: "", movieID: 1, releaseDate: "", averageRating: 0.0)
+        XCTAssertNotEqual(first, second)
+    }
+
+}
